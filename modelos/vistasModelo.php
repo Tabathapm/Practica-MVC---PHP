c4ffbe85350f7ebf4c4e6b27ed4924140ef95c7e
Tabathapm <tabathapm@gmail.com>
2021-10-16 21:04:31 -0300

    Se agregan más vistas en 'contenidos'

--- modelos/vistasModelo.php
+++ modelos/vistasModelo.php
@@ -3,7 +3,10 @@
 
         // ------ Modelo para obtener las vistas -----
         protected static function obtener_vistas_modelo($vistas){
-            $listaBlanca = ["home", "client-list", "client-new"];
+            $listaBlanca = ["home", "client-list", "client-new", "client-search", "clien-update", "company",
+                "item-list",  "item-new", "item-search", "item-update", "reservation-list", "reservation-new",
+                "reservation-pending", "reservation-reservation", "reservation-search", "reservation-update",
+                "user-list", "user-new", "user-search", "user-update"];
 
             if (in_array($vistas, $listaBlanca)){ //Si está dentro de nuestra lista de permitidos
                 if (is_file("./vistas/contenidos/".$vistas."-view.php")){ //si encuentra este archivo
@@ -20,3 +23,5 @@
             return $contenido;
         }
     }
+
+
